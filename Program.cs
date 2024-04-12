cec6a62745575ece571cc2ba6e24b766fcd14f04
MarkIvanchenko <158240906+MarkIvanchenko@users.noreply.github.com>
2024-04-12 12:43:27 +0300

    Код самой программы

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите количество строк в массиве:");
+int count = int.Parse(Console.ReadLine());
+
+
+    string[] array = new string[count];
+    
+    for (int i = 0; i < count; i++)
+    {
+        Console.WriteLine($"Введите строку {i + 1}:");
+        array[i] = Console.ReadLine();
+    }
+
+    Console.WriteLine("Строки с длиной меньше 4 символов:");
+
+    foreach (string str in array)
+    {
+        if (str.Length < 4)
+        {
+            Console.WriteLine(str);
+        }
+    }
